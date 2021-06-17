b8b6e7c5d3cbc9037f338742129808fd922ec9c4
Alekhya0407 <84900508+Alekhya0407@users.noreply.github.com>
2021-06-17 11:03:22 +0530

    Delete project directory

--- project/routes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express'); 
-
-function eRoutes() {
-    const router = express.Router();
-    var employee = require('./repository/employee/employee.routes')(router);
-    return router;
-}
-
-module.exports = eRoutes;
--- project/shared/response.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(data, error){
-    if(error){
-        return error;
-    }
-    else
-    return data;
-}
